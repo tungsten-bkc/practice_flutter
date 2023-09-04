c37eafd853355031b6d7bcec67241e09684d2729
tungsten-bkc <takumi.volley823@gmail.com>
2023-09-04 22:18:10 +0900

    add const

---
+++ practice_flutter/lib/const/charactor.dart
@@ -0,0 +1,23 @@
+class Charactor {
+  static Map characters = {
+    0: {'name': 'ryu', 'image': 'ryu.png'},
+    1: {'name': 'luke', 'image': 'luke.png'},
+    2: {'name': 'jamie', 'image': 'jamie.png'},
+    3: {'name': 'chun-li', 'image': 'chunli.png'},
+    4: {'name': 'guile', 'image': 'guile.png'},
+    5: {'name': 'kimberly', 'image': 'kimberly.png'},
+    6: {'name': 'juri', 'image': 'juri.png'},
+    7: {'name': 'ken', 'image': 'ken.png'},
+    8: {'name': 'blanka', 'image': 'blanka.png'},
+    9: {'name': 'dhalsim', 'image': 'dhalsim.png'},
+    10: {'name': 'e.honda', 'image': 'ehonda.png'},
+    11: {'name': 'dee jay', 'image': 'deejay.png'},
+    12: {'name': 'manon', 'image': 'manon.png'},
+    13: {'name': 'marisa', 'image': 'marisa.png'},
+    14: {'name': 'jp', 'image': 'jp.png'},
+    15: {'name': 'zangief', 'image': 'zangief.png'},
+    16: {'name': 'lily', 'image': 'lily.png'},
+    17: {'name': 'cammy', 'image': 'cammy.png'},
+    18: {'name': 'rashid', 'image': 'rashid.png'},
+  };
+}
